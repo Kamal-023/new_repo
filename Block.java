5e5607c8d7e965e12fa1496eb371d7c916afb6df
Kamal <23b81a7223@cvr.ac.in>
2025-03-03 14:15:52 +0530

    Block.java

---
+++ Block.java
@@ -0,0 +1,18 @@
+class Block{
+ static{
+   int a=20;
+   int b=30;
+        int c=a+b;
+    System.out.println("The addition of"+a+"and"+b+"is"+c);
+   }
+ 
+public static void main(String[] args){
+ System.out.println("Main method");
+{
+    int d=40;
+    int e=20;
+    int f=d-e;
+    System.out.println("The subtraction of"+d+"and"+e+"is"+f);
+}
+}
+}
---
+++ Byte.java
@@ -0,0 +1,21 @@
+import java.FileInputStream;
+import java.OutputStream;
+import java.IOException;
+public class Byte
+{
+public static void main(String[] args)
+{
+int data;
+try
+{
+(FileInputStream fis=new FileInputStream("hello");
+(File OutInputStream os=new OutInputStream("world");
+while((data=fis.read)!=-1)
+{
+System.out.println("executed");
+catch(IOException e)
+{
+e.printStackTrace();
+}
+}
+}
